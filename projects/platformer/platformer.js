7579ba71426df767fa7921dc63794767e1b74a97
K'Tyra Knox <ktyknox@tangistudents.org>
2023-09-19 14:34:02 +0000

    added platforms and collectables to game

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -44,27 +44,32 @@ $(function () {
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
 
-createPlatform(100, 300, 200, 20);
-createPlatform()
+createPlatform(200, 610, 200, 20);
+createPlatform(410, 510, 200, 20);
+createPlatform(100, 410, 200, 20);
+createPlatform(510, 310, 200, 20);
+createPlatform(900, 210, 200, 20);
+createPlatform(820, 410, 200, 20);
+createPlatform(920, 610, 200, 20);
+createPlatform(1100, 320, 100, 20);
 
-    
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
-
+createCollectable("steve", 600, 200, 20, 0.5);
+createCollectable("kennedi", 1000, 500, 20, 0.5);
+createCollectable("grace", 1120, 100, 20, 0.5);
 
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
-
-
+createCannon("left", 400, 1000);
+createCannon("right", 200, 1000);
+createCannon("top", 700, 1000);
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
